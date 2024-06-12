a39db5e590c23fabcd703d0140eeab9f538968f1
mxschll <max@schoell.co>
2024-06-12 19:46:54 +0200

    add 00-essentials.sh

---
+++ install/00-essentials.sh
@@ -0,0 +1,8 @@
+sudo apt install -y git 
+sudo apt install -y ripgrep
+
+# Build prerequisites (eg. for building Neovim)
+sudo apt-get install -y ninja-build gettext cmake unzip curl build-essential
+
+
+
