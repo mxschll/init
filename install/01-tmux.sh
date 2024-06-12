8880b5deac72695522415bfd0714c967e8e6b6ac
mxschll <max@schoell.co>
2024-06-12 20:05:40 +0200

    fix: tmux config path

--- install/01-tmux.sh
+++ install/01-tmux.sh
@@ -2,4 +2,4 @@ sudo apt install -y tmux
 
 # Config 
 [ -f "~/.tmux.conf" ] && mv ~/.tmux.conf ~/.tmux.conf.bak
-cp ~/.local/share/init/config/tmux/.tmux.conf ~/.config/.tmux.conf
+cp ~/.local/share/init/config/tmux/.tmux.conf ~/.tmux.conf
