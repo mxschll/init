1d2acb57e9787108a6d19024ea21c508a85fba07
mxschll <max@schoell.co>
2024-06-12 20:50:56 +0200

    config tmux autostart in .szhrc

--- install/01-tmux.sh
+++ install/01-tmux.sh
@@ -1,5 +1,10 @@
 sudo apt install -y tmux
 
 # Config 
-[ -f "~/.tmux.conf" ] && mv ~/.tmux.conf ~/.tmux.conf.bak
+[ -f ~/.tmux.conf ] && mv ~/.tmux.conf ~/.tmux.conf.bak
 cp ~/.local/share/init/config/tmux/.tmux.conf ~/.tmux.conf
+
+if [ -f ~/.zshrc ]; then
+	sed -i '/source $ZSH\/oh-my-zsh.sh/i ZSH_TMUX_AUTOSTART=true\nZSH_TMUX_AUTOCONNECT=false\n' ~/.zshrc
+fi
+
