d2c1102132e0ae48d7e50e1872ea1a96154398dc
mxschll <max@schoell.co>
2024-06-13 16:56:12 +0200

    add install script for enpass

---
+++ install/04-enpass.sh
@@ -0,0 +1,4 @@
+# Enpass is a password manager
+echo "deb https://apt.enpass.io/  stable main" | sudo tee /etc/apt/sources.list.d/enpass.list
+wget -O - https://apt.enpass.io/keys/enpass-linux.key | sudo tee /etc/apt/trusted.gpg.d/enpass.asc
+sudo apt update && sudo apt install -y enpass
