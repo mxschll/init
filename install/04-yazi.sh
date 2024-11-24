d75b8b110bc9a6368d995a572abdfe65f62fb9c2
Maximilian Schöll <max@schoell.co>
2024-11-24 13:03:37 +0000

    fix: use standard rustup installation

--- install/04-yazi.sh
+++ install/04-yazi.sh
@@ -1,5 +1,6 @@
 # Setup the latest stable Rust toolchain via rustup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 rustup update
 
 # Install the latest Git version of yazi
