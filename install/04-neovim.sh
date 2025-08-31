d21262fd14665fa2c4f03f4deee1dfcf9b3ea73e
Maximilian Schöll <max@schoell.co>
2025-09-01 00:28:05 +0200

    chore: rewrite for arch

--- install/04-neovim.sh
+++ install/04-neovim.sh
@@ -1,11 +1,17 @@
+# Install dependencies for building Neovim
+sudo pacman -Syu --noconfirm base-devel cmake unzip ninja tree-sitter curl
+
+# Clone Neovim
 git clone https://github.com/neovim/neovim ~/Downloads/neovim
 cd ~/Downloads/neovim
 git checkout stable
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
-# Config: Clone nvim kickstart config
-git clone https://github.com/mxschll/kickstart.nvim.git ~/.config/nvim
+# Build
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+
+# Install (no .deb on Arch, just use make install)
+sudo make install
+# cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
 
 # Cleanup
 cd ~
